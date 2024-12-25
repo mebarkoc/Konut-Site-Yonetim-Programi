cba560f4d9eef1aec367d3b9d6775062f40d9dfa
Muhammed Emin BARKOÇ <eminbarkoc@gmail.com>
2024-12-25 23:24:09 +0300

    Başlangıç

---
+++ Konut Site Yönetim Programı/Konut Site Yönetim Programı.cpp
@@ -0,0 +1,23 @@
+﻿/****************************************************************************
+**                        SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				     BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**					   PROGRAMLAMAYA GİRİŞ DERSİ
+**						2024-2025 GÜZ DÖNEMİ
+**
+**				ÖDEV NUMARASI..........: PROJE ÖDEVİ
+**				ÖĞRENCİ ADI............: MUHAMMED EMİN BARKOÇ
+**				ÖĞRENCİ NUMARASI.......: G231210452
+**				DERSİN ALINDIĞI GRUP...: İKİNCİ ÖĞRETİM A GRUBU
+**
+*****************************************************************************/
+
+#include <iostream>
+
+
+
+
+
+int main()
+{
+}
